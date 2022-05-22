f7b8031af509df60ca3618b5cba5f6013cfd5d94
Kevin Hathaway <kevinatmnstr@gmail.com>
2022-05-22 17:38:02 -0400

    update for parallels provider and ubuntu 20.04

--- exercises/scripts/common.sh
+++ exercises/scripts/common.sh
@@ -4,26 +4,32 @@ if [ -z ${K8S_VERSION+x} ]; then
   K8S_VERSION=1.23.4-00
 fi
 
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-apt-get update
+apt-get update -y && apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
 apt-get install -y \
   avahi-daemon \
   libnss-mdns \
   traceroute \
   htop \
   httpie \
-  bash-completion \
-  docker-ce=5:19.03.14~3-0~ubuntu-bionic \
+  bash-completion 
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update -y
+apt-get install -y \
   kubeadm=$K8S_VERSION \
   kubelet=$K8S_VERSION \
   kubectl=$K8S_VERSION
+
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
